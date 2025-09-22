52745b04c431d0eafe2f67c05bd2deb0e7d4e71a
Dikshant Arora <aroradikshant11@gmail.com>
2025-09-22 16:51:26 +0530

    Add files via upload

---
+++ Day23/Day23_Q46.c
@@ -0,0 +1,28 @@
+// Q46: Write a program to print the following pattern:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+
+// Sample Test Cases:
+// Input 1:
+
+// Output 1:
+// *****
+// *****
+// *****
+// *****
+// *****
+
+#include<stdio.h>
+int main(){
+    for(int j=1;j<=5;j++){
+        for(int i=1;i<=5;i++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
