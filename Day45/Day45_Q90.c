3e69c66d869e7287bd7efa551fd54e8beb91c729
Dikshant Arora <aroradikshant11@gmail.com>
2025-11-04 11:15:24 +0530

    Update Day45_Q90.c
    This program toggles the case of each character in a string input by the user.

---
+++ Day45/Day45_Q90.c
@@ -0,0 +1,21 @@
+//Q90: Toggle case of each character in a string.
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isupper(str[i])) {
+            str[i] = tolower(str[i]);
+        } else if (islower(str[i])) {
+            str[i] = toupper(str[i]);
+        }
+    }
+
+    printf("Modified string: %s\n", str);
+    return 0;
+}
